70c6f8cea17dcfc0a3e4cc502e14874083e19fb7
--replace-all <2361575m@student.gla.ac.uk>
2022-02-18 00:52:29 +0000

    completed chapter 4 exercises

--- rango/views.py
+++ rango/views.py
@@ -11,6 +11,13 @@ def index(request):
     # We make use of the shortcut functioon to make our lives easier.
     # Note that the first parameter is the template we wish to use.
     return render(request, 'rango/index.html', context=context_dict)
-    
+
 def about(request):
-    return HttpResponse("Rango says here is the about page."+'<a href="/rango/">Index</a>')
+    # Construct a dictionary to pass to the template engine as its context.
+    # Note the key boldmessagee matches to {{ boldmessage }} in the template!
+    context_dict = {'boldmessage': 'This tutorial has been put together by Zainab'}
+
+    # Return a rendered response to send to the client.
+    # We make use of the shortcut functioon to make our lives easier.
+    # Note that the first parameter is the template we wish to use.
+    return render(request, 'rango/about.html', context=context_dict)
